8f1fe4e246b3a14a53bb78eadcf220c92fd36f12
sandroschutt <68920430+sandroschutt@users.noreply.github.com>
2022-02-25 17:40:25 -0300

    Create exercicio_03.py

---
+++ Estrutura_de_dados/listas/lista-pré-prova/exercicio_03.py
@@ -0,0 +1,22 @@
+def calculaColisao(cenario):
+    colide = []
+    for i in range(4):
+        if cenario[1][x] - cenario[0][x] >= 2:
+            saida = 0
+            colide.append(saida)
+        else:
+            saida = 1
+            colide.append(saida)
+    colisao = sum(colide)
+    if colisao == 3 or colisao == 4:
+        print(1)
+    else:
+        print(0)
+
+cenario1 = [[0, 0, 1, 1],[0, 0, 1, 1]]
+cenario2 = [[0, 0, 2, 2],[1, 1, 3, 3]]
+cenario3 = [[0, 0, 1, 1],[2, 2, 3, 3]]
+
+calculaColisao(cenario1)
+calculaColisao(cenario2)
+calculaColisao(cenario3)
